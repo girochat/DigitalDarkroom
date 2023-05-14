0fb02f6ef0a16657bc9e589b0680bd7cb9683300
Giliane Rochat <giliane.rochat@students.unibe.ch>
2023-05-15 00:19:18 +0200

    image_upload.py - SystemExit added

--- DigitalDarkroom/image_upload.py
+++ DigitalDarkroom/image_upload.py
@@ -30,6 +30,7 @@ choose_file_type
 import os
 import shutil
 import glob
+import config
 import numpy as np
 import pandas as pd
 from extract_metadata import extract_metadata_upload
@@ -52,7 +53,7 @@ class Event():
         self.name = name
         self.description = description
         if self.name != "":
-            self.path = os.path.join(os.environ["IMAGES_PATH"], self.name)
+            self.path = os.path.join(config.images_path, self.name)
         else:
             self.path = None
         print("Your event has been created!\n")
@@ -71,23 +72,28 @@ def create_event():
 
     # Create event folder in DigitalDarkroom
     try:
-        event_path = os.path.join(os.environ["IMAGES_PATH"], event_name)
+        event_path = os.path.join(config.images_path, event_name)
         if not os.path.exists(event_path):
             os.makedirs(event_path)
+        else:
+            print("Sorry, the event already exists...")
+            
     except Exception:
         print("Error! You must enter a valid name to create the event folder...")
+        raise SystemExit
 
     return Event(name = event_name, description = event_description)
 
 def single_file(source, dest):
     """ Copies a single image to Digital Darkroom.
     """
-    filename = input("Enter the filename of the image you would like to upload: (Q for Quit)\n")
+    filename = input("Enter the filename of the image you would like to upload: (Q/Quit)\n")
     print()
 
     if filename.lower() in ['q', "quit"]:
         print("You decided to stop the image upload.")
-        return
+        raise SystemExit
+        
     path_to_file = os.path.join(source, filename)
 
     # Check if results are files
@@ -96,50 +102,46 @@ def single_file(source, dest):
         print("Image has been copied!\n")
     else:
         print("Sorry, the image could not be found... Upload aborted.")
-        return
+        raise SystemExit
 
-
-# if user is in folder with his images, ask the user for file extension
-# copy all images in folder with specific file ending -> which file endings should be available?
-# Issue: Select all extensions will also import text files (needs to be changed) -> Done
-# Question: use select or switch instead? Or let user just enter one or several extensions?
-# Add option to add a list of images?
 def choose_file_type(source, dest):
     """ Copies an entire list of images based on the image extension to Digital Darkroom.
     """
     options = ["all", "png", "jpg", "jpeg", "cr2", "nef", "tif", "bmp", "other"]
-    user_input = ''
-    input_message = input("What files would you like to upload? "
-                          "Press enter and pick an option by entering the number "
-                          "or typing the file extension. Press q to quit.")
-    print()
-
-    if input_message.lower() == 'q':
-        print("You decided to stop the image upload.")
-        return
-
-    for index, item in enumerate(options):
-        input_message += f'{index+1}) {item}\n'
-        index_list = [str(i) for i in range(1, len(options) + 1)]
-    while (user_input not in options) and (user_input not in index_list):
-        user_input = input(input_message)
-    if user_input in ["all", "1"]:
-        file_extension = '*.*'
-    elif user_input == 'other' or user_input == str(len(options)):
-        user_input = input("Enter the extension of the images you would like to add: ")
+    index_list = [str(i) for i in range(1, len(options) + 1)]
+    extensions_display = "\n".join([f'{index+1}) {item}' for index, item in enumerate(options)])
+    
+    # Display possible extensions to the user
+    answer = False
+    while (answer not in options) and (answer not in index_list):
+        print(extensions_display)
+        answer = input("What files would you like to upload? "
+                       "Pick an option by entering the number "
+                       "or typing the file extension: (Q/Quit)\n").lower()
         print()
-        file_extension = '*.' + user_input
-    elif user_input.isdigit():
-        ending = options[int(user_input)-1]
-        file_extension = '*.' + ending
-    else:
-        file_extension = '*.' + user_input
 
+        if answer in ['q', "quit"]:
+            raise SystemExit
+        elif answer in ["all", "1"]:
+            file_extension = '*.*'
+        elif answer == 'other' or answer == str(len(options)):
+            user_input = input("Enter the extension of the images you would like to add: ")
+            print()
+            file_extension = '*.' + user_input
+        elif answer.isdigit():
+            ending = options[int(answer)-1]
+            file_extension = '*.' + ending
+        else:
+            file_extension = '*.' + answer
+
+    # copy whole tree (with subdirs), glob.glob("path/to/dir/*.*") to get list of all filenames
     for full_file_name in glob.glob(os.path.join(source, file_extension)):
-        # copy whole tree (with subdirs), glob.glob("path/to/dir/*.*") to get list of all filenames
         file_name = os.path.basename(full_file_name)
-        if os.path.isfile(full_file_name): # check if results are files
+        
+        # Copy only files from given directory
+        if os.path.isfile(full_file_name): 
             extract_metadata_upload(full_file_name, dest, file_name)
+    
     print("Images have been copied!\n")
 
 def upload_images():
@@ -165,26 +167,36 @@ def upload_images():
                                " or add them to an existing event?"
                                " (C/Create or A/Add or N/No or Q/Quit) :\n").lower()
         print()
+        
         if event_creation in ["c", "create"]:
             event = create_event()
             upload_to = event.path
             answer = True
+            
         elif event_creation in ["a", "add"]:
-            list_event = np.unique(pd.read_pickle(os.path.join(os.environ["PROGRAM_PATH"], "image_DB.pkl"))["Event"].dropna())
+            
+            # Display list of available events
+            list_event = np.unique(config.DB["Event"].dropna())
             for event_name in list_event:
                 print(event_name)
             event = input("To which event from the given list would you like to add images:\n")
             print()
-            upload_to = os.path.join(os.environ["IMAGES_PATH"], event)
+            
+            upload_to = os.path.join(config.images_path, event)
             answer = True
+            
+            # Check that the event exists in Images
             if not os.path.exists(upload_to):
                 print("Sorry, the event was not found... \n")
                 answer = False
+                
         elif event_creation in ["n", "no"]:
+            upload_to = config.images_path
             answer = True
-            upload_to = os.environ["IMAGES_PATH"]
+            
         elif event_creation in ["q", "quit"]:
             raise SystemExit
+            
         else:
             print("Error! Please enter one of the valid options as displayed...")
 
