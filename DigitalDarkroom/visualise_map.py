0feac1e4e0ba9605d9ede3ccdb4a4b1896c887dd
Giliane Rochat <giliane.rochat@students.unibe.ch>
2023-05-24 18:31:57 +0200

    visualise_map.py - synchronised with DB

--- DigitalDarkroom/visualise_map.py
+++ DigitalDarkroom/visualise_map.py
@@ -1,11 +1,11 @@
 """ Module to visualise picture locations on a world map.
 """
 import os
+import config
 import numpy as np
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
-import config
 import display_images as implay
 
 def select_images(pick_event):
@@ -22,11 +22,11 @@ def select_images(pick_event):
     
     # Launch image diaporama if right click 
     if pick_event.mouseevent.button == 1:
-        implay.display_diaporama(images)
+        implay.display_diaporama(images, picker = False)
     
     # Launch image panorama if left click
     elif pick_event.mouseevent.button == 3:
-        implay.display_panorama(images)
+        implay.display_panorama(images, picker = False)
 
 def plot_locations():
     """ Plot image locations on world map.
@@ -39,14 +39,13 @@ def plot_locations():
     world_map.plot(ax = axis, column = 'LABEL_Y', cmap = "viridis")
     axis.set_axis_off()
 
-    # Create geopandas dataframe
-    #locations = gpd.GeoSeries([Point(image_DB.loc[image, "Latitude"], image_DB.loc[image, "Longitude"]) for image in image_DB.index])
+    # Create geopandas dataframe with the image locations
+    geodata_locations = gpd.GeoDataFrame(config.DB[["Latitude", "Longitude", "Location"]], 
+                                         geometry = gpd.points_from_xy(config.DB["Longitude"], 
+                                                                       config.DB["Latitude"]))
+
 
     # Plot image locations
-    #locations = config.DB["Event"]                      # just to test, normally it's the locations in image_DB.pkl
-    #geodata_locations = gpd.tools.geocode(locations)    # just to test, normally it's the locations in image_DB.pkl
-    #geodata_locations.to_file("temp.shp")
-    geodata_locations = gpd.GeoDataFrame.from_file("temp.shp")
     geodata_locations.plot(ax = axis, color = "red", picker = True)
     plt.connect(s = "pick_event", func = select_images)
     plt.show()
