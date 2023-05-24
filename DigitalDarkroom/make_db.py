6749acdda522020c6b85655471fa6af05832cdd9
michaela.amherd@students.unibe.ch <amherd.michaela@students.unibe.ch>
2023-05-24 18:07:47 +0200

    column edited set to False at image upload

--- DigitalDarkroom/make_db.py
+++ DigitalDarkroom/make_db.py
@@ -2,6 +2,6 @@ import os
 import pandas as pd
 import config
 config.DB = pd.DataFrame(columns = ['Event', 'Format', 'Width', 'Height','Megapixels','Channels'
-,'Mode','Timestamp', 'Creation','Date_Time','Date','Latitude','Longitude',
-'Location','Edited'])
+,'Mode','Timestamp', 'Creation','Date_Time','Date','Edited','Latitude','Longitude',
+'Location'])
 config.DB.to_pickle(os.path.join(config.program_path, "image_DB.pkl")) 
